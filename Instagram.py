0e379c499aef39d020be16f86ce70d157d76291e
m00re <jens.mittag@gmail.com>
2016-09-26 22:01:05 +0200

    Minor modifications in SQL schema, and implementation of scrape result persistence

--- Instagram.py
+++ Instagram.py
@@ -1,7 +1,19 @@
-from contextlib import contextmanager
 import requests
 import re
+import logging
+import time
 import json
+
+module_logger = logging.getLogger('instagram')
+mysql_logger = logging.getLogger('mysql')
+ch = logging.StreamHandler()
+formatter = logging.Formatter('%(asctime)s %(levelname)s - %(name)s: %(message)s')
+module_logger.setLevel(logging.INFO)
+mysql_logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+module_logger.addHandler(ch)
+mysql_logger.addHandler(ch)
 
 def extract_tags(caption):
     tags = []
@@ -9,7 +21,6 @@ def extract_tags(caption):
         tags.append(m.group(1))
     return tags
 
-@contextmanager
 def scrape(profilename):
 
     # Prepare result hash
@@ -28,6 +39,7 @@ def scrape(profilename):
         result['user_username'] = user['username']
         result['follower'] = user['followed_by']['count']
         result['follows'] = user['follows']['count']
+        result['media_count'] = user['media']['count']
         result['media'] = []
 
         for post in user['media']['nodes']:
@@ -45,5 +57,72 @@ def scrape(profilename):
             result['media'].append(post)
 
         result['success'] = 1
+        module_logger.info('Scraping successful with ' + str(result['follower']) + ' followers and ' + str(len(result['media'])) +
+                           ' posts for \'' + result['user_username'] + '\'.')
+    else:
+        module_logger.error('Failed to extract meta-information from HTML page')
 
-    yield result
+    return result
+
+def persist(result, mysql_cnx):
+    # Step 1: get profile id from existing database record
+    query = ("SELECT id FROM profiles WHERE network='instagram' AND profile_name=%s")
+    cursor = mysql_cnx.cursor()
+    cursor.execute(query, (result['user_username'], ))
+    profile = cursor.fetchone()
+
+    if profile is None:
+        module_logger.error('No matching profile found for the given result object')
+        return -1
+
+    # Step 2: insert new profile stats record
+    insert_query = ("INSERT INTO profile_stats (profile, followers, follows, posts) VALUES (%s, %s, %s, %s)")
+    cursor.execute(insert_query, (profile[0], result['follower'], result['follows'], result['media_count']))
+
+    # Step 3: iterate over all posts
+    for post in result['media']:
+        query = ("SELECT id FROM posts WHERE profile=%s AND osn_id=%s")
+        cursor.execute(query, (profile[0], post['id']))
+        row = cursor.fetchone()
+
+        if row is None:
+            # if a new post record has been created...
+            insert_query = ("INSERT INTO posts "
+                            "(profile, osn_id, creation, thumbnail_src, display_src, is_video, caption) "
+                            "VALUES "
+                            "(%s, %s, %s, %s, %s, %s, %s)")
+            time_struct = time.gmtime(post['date'])
+            time_formatted = time.strftime('%Y-%m-%d %H:%M:%S', time_struct)
+            cursor.execute(insert_query, (profile[0], post['id'], time_formatted, post['thumbnail'], post['image'], post['is_video'], post['caption']))
+            post_id = cursor.lastrowid
+            mysql_logger.debug('Created new post record with id ' + str(post_id))
+
+            # and also insert and link tag records
+            for tag in post['tags']:
+                # check if tag name already exists in database
+                query = ("SELECT id FROM tags WHERE label=%s")
+                cursor.execute(query, (tag, ))
+                tag_row = cursor.fetchone()
+                if tag_row is None:
+                    insert_query = ("INSERT INTO tags (label) VALUES (%s)")
+                    cursor.execute(insert_query, (tag, ))
+                    tag_id = cursor.lastrowid
+                    mysql_logger.debug('Created new record for tag \'#' + tag + '\' with id ' + str(tag_id))
+                else:
+                    tag_id = tag_row[0]
+                    mysql_logger.debug('Found existing record for tag \'#' + tag + '\'')
+
+                # link tag to post
+                insert_query = ("INSERT INTO posts_tags_mm (post_id, tag_id) VALUES (%s, %s)")
+                cursor.execute(insert_query, (post_id, tag_id))
+        else:
+            post_id = row[0]
+            mysql_logger.debug('Found existing post with id ' + str(post_id))
+
+        # insert new post stats record
+        insert_query = ("INSERT INTO post_stats (post, comments, likes) VALUES (%s, %s, %s)")
+        cursor.execute(insert_query, (post_id, post['comments'], post['likes']))
+
+    # Make sure everything is committed
+    mysql_cnx.commit()
+    cursor.close()
--- app.py
+++ app.py
@@ -1,22 +1,69 @@
 import Instagram
 import logging
+import mysql.connector
+import atexit
+from mysql.connector import errorcode
 from flask import Flask, jsonify
 from apscheduler.schedulers.background import BackgroundScheduler
 
-app = Flask(__name__)
-
+# Setup logging facilities
 logger = logging.getLogger('app')
+schedulerlog = logging.getLogger('apscheduler.executors.default')
 ch = logging.StreamHandler()
 formatter = logging.Formatter('%(asctime)s %(levelname)s - %(name)s: %(message)s')
 logger.setLevel(logging.INFO)
 ch.setLevel(logging.INFO)
 ch.setFormatter(formatter)
 logger.addHandler(ch)
+schedulerlog.addHandler(ch)
+
+# Setup MySQL database connection
+try:
+    cnx = mysql.connector.connect(user='instagram',
+                                  password='aj2IcPqoFmuir9MLKPB3',
+                                  host='localhost',
+                                  port=3306,
+                                  database='instagram_monitor',
+                                  use_pure=False)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        logger.error('Access denied when connecting to MySQL database.')
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        logger.error('The requested database \'instagram_monitor\' does not exist.')
+    else:
+        logger.error(err)
+
 
 def scrape():
     logger.info('Starting to scrape...')
-    Instagram.scrape('lefrogfotografie')
+
+    query = ("SELECT id, network, profile_name FROM profiles")
+    cursor = cnx.cursor()
+    cursor.execute(query)
+    profiles = cursor.fetchall()
+
+    for profile in profiles:
+
+        if profile[1] == "instagram":
+            logger.info('-> scraping instagram profile \'' + profile[2] + '\'...')
+            result = Instagram.scrape(profile[2])
+            Instagram.persist(result, cnx)
+
+    cnx.commit()
+    cursor.close()
+
     logger.info('...finished!')
+
+def close_database():
+    logger.info('Closing database connection')
+    cnx.close()
+
+def ctrl_c_handler(signum, frame):
+    logger.info('CTRL-C received, shutting down database connection')
+    close_database()
+
+# Flask Application Setup
+app = Flask(__name__)
 
 # Returns a list of supported networks
 @app.route('/networks', methods=['GET'])
@@ -39,5 +86,7 @@ def profiles(network):
 if __name__ == '__main__':
     scheduler = BackgroundScheduler()
     scheduler.start()
+    scheduler.add_job(scrape)
     scheduler.add_job(scrape, 'interval', minutes=5)
+    atexit.register(close_database)
     app.run()
